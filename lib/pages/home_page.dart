a06e32a571d931a354a21a15f125c5ab8448fb28
MalithKeerthisinghe <102132303+MalithKeerthisinghe@users.noreply.github.com>
2024-12-30 22:56:27 +0530

    adding more dart files and images
    models
    1. food.dart
    components
    1. my_description_box.dart
    2. my_tab_bar.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_current_location.dart';
+import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
 import 'package:food_delivery_app/components/my_silver_app_bar.dart';
+import 'package:food_delivery_app/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,7 +12,23 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +36,7 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) => [
                 MySilverAppBar(
-                  title: Text("title"),
+                  title: MyTabBar(tabController: _tabController),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
@@ -28,14 +47,30 @@ class _HomePageState extends State<HomePage> {
                       ),
 
                       // my current location
+                      const MyCurrentLocation(),
 
                       // description box
+                      const MyDescriptionBox(),
                     ],
                   ),
                 ),
               ],
-          body: Container(
-            color: Colors.blue,
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("first tab items"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("second tab items"),
+              ),
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("third tab items"),
+              ),
+            ],
           )),
     );
   }
