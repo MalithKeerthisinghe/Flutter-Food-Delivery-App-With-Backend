3b980d48a214c7a77fac24a6f6f554dfeb6e1532
MalithKeerthisinghe <102132303+MalithKeerthisinghe@users.noreply.github.com>
2024-12-31 20:53:59 +0530

    Adding some pages and categories
    my_food_tile.dart
    restaurant.dart

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,379 @@
+import 'package:flutter/material.dart';
+
+import 'food.dart';
+
+class Restaurant extends ChangeNotifier {
+  // list of food menu
+  final List<Food> _menu = [
+    // burgers
+    Food(
+      name: "Cheese Burger",
+      description:
+          "A juicy beef patty with melted cheddar, lettuce, tomato, and a hint of onion and pickle.",
+      imagePath: "lib/images/burger/cheeseburger.jpeg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Chicken Burger",
+      description:
+          "Smoky BBQ sauce, crispy bacon, and onion rings make this burger a savory delight",
+      imagePath: "lib/images/burger/chikenburger.jpeg",
+      price: 10.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Grilled Onions", price: 0.99),
+        Addon(name: "Jalapenos", price: 1.49),
+        Addon(name: "Extra BBQ sauce", price: 1.99),
+      ],
+    ),
+    Food(
+      name: "Hamburger",
+      description:
+          "Juicy ham, tangy mustard, and melted Swiss cheese come together in this irresistible burger classic.",
+      imagePath: "lib/images/burger/hamburger.jpeg",
+      price: 9.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Grilled Mushrooms", price: 1.49),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Onion Burger",
+      description:
+          "Caramelized onions, zesty aioli, and gooey cheddar make this burger an onion lover’s dream.",
+      imagePath: "lib/images/burger/onionburger.jpeg",
+      price: 9.49,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra onion", price: 0.99),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Turkey Burger",
+      description:
+          "Herbed turkey patty, creamy avocado, and a touch of cranberry sauce create a flavorful twist on this turkey burger.",
+      imagePath: "lib/images/burger/turkeyburger.jpeg",
+      price: 0.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Extra avocado", price: 1.49),
+        Addon(name: "Extra cranberry sauce", price: 0.99),
+      ],
+    ),
+
+    // salads
+    Food(
+      name: "Caesar Salad",
+      description:
+          "Crisp romaine, creamy Caesar dressing, crunchy croutons, and shaved Parmesan make this salad a timeless classic.",
+      imagePath: "lib/images/salads/caesarsalad.jpeg",
+      price: 7.99,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Grilled Chicken", price: 0.99),
+        Addon(name: "Anchovies", price: 1.49),
+        Addon(name: "Extra Parmesan", price: 1.99),
+      ],
+    ),
+    Food(
+      name: "Cobb Salad",
+      description:
+          "Fresh greens, tender chicken, crispy bacon, creamy avocado, hard-boiled eggs, and tangy blue cheese crumble into this hearty Cobb salad delight.",
+      imagePath: "lib/images/salads/cobbsalad.jpeg",
+      price: 8.49,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra Bacon", price: 0.99),
+        Addon(name: "Tender Chicken", price: 1.49),
+        Addon(name: "Extra avocado", price: 1.49),
+      ],
+    ),
+    Food(
+      name: "Fattoush",
+      description:
+          "Fresh greens, juicy tomatoes, crunchy cucumbers, crispy pita chips, and a zesty sumac dressing make this Fattoush a vibrant Mediterranean favorite.",
+      imagePath: "lib/images/salads/fattoush.jpeg",
+      price: 7.99,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra tomatoes", price: 2.99),
+        Addon(name: "Extra cucumbers", price: 3.99),
+        Addon(name: "Extra pita chips", price: 1.99),
+      ],
+    ),
+    Food(
+      name: "Greek Salad",
+      description:
+          "Crunchy cucumbers, ripe tomatoes, tangy feta, briny olives, and a drizzle of olive oil bring the Mediterranean to your plate with this classic Greek salad.",
+      imagePath: "lib/images/salads/greeksalad.jpeg",
+      price: 5.99,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra tomatoes", price: 2.99),
+        Addon(name: "Extra olives", price: 0.99),
+        Addon(name: "Extra cucumbers", price: 3.99),
+      ],
+    ),
+    Food(
+      name: "Green Salad",
+      description:
+          "Fresh mixed greens, crisp cucumbers, juicy cherry tomatoes, and a light vinaigrette make this green salad a refreshing and simple delight.",
+      imagePath: "lib/images/salads/greensalad.jpeg",
+      price: 7.99,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra cherry", price: 1.99),
+        Addon(name: "Extra tomatoes", price: 2.99),
+        Addon(name: "Extra cucumbers", price: 3.99),
+      ],
+    ),
+
+    // sides
+    Food(
+      name: "Coleslaw",
+      description:
+          "Crisp shredded cabbage, sweet carrots, and a creamy tangy dressing make this coleslaw a classic, refreshing favorite.",
+      imagePath: "lib/images/sides/coleslaw.jpeg",
+      price: 5.99,
+      category: FoodCategory.sides,
+      availableAddons: [
+        Addon(name: "Extra carrots", price: 2.99),
+        Addon(name: "Extra cabbage", price: 1.99),
+        Addon(name: "Tangy", price: 0.99),
+      ],
+    ),
+    Food(
+      name: "French Fries",
+      description:
+          "Golden, crispy fries with a perfect balance of salt make these French fries the ultimate snack or side dish.",
+      imagePath: "lib/images/sides/frenchfries.jpeg",
+      price: 4.99,
+      category: FoodCategory.sides,
+      availableAddons: [
+        Addon(name: "Extra fries", price: 1.99),
+        Addon(name: "Extra Mayo", price: 1.49),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Roasted Carrots",
+      description:
+          "Sweet, tender roasted carrots with a caramelized glaze and a hint of earthy herbs make this side dish a flavorful delight.",
+      imagePath: "lib/images/sides/roastedcarrots.jpeg",
+      price: 3.99,
+      category: FoodCategory.sides,
+      availableAddons: [
+        Addon(name: "Extra carrots", price: 1.99),
+        Addon(name: "Extra Mayo", price: 1.49),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Roasted Vegetables",
+      description:
+          "Vibrant roasted vegetables, caramelized to perfection with a blend of savory herbs, offer a delicious and hearty side dish.",
+      imagePath: "lib/images/sides/roastedvegetables.jpeg",
+      price: 5.99,
+      category: FoodCategory.sides,
+      availableAddons: [
+        Addon(name: "Extra herbs", price: 2.99),
+        Addon(name: "Extra Mayo", price: 1.49),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Skillet Corn",
+      description:
+          "Sweet, tender corn cooked to perfection in a skillet, with a touch of butter and seasonings, makes this dish a savory, satisfying treat.",
+      imagePath: "lib/images/sides/skilletcorn.jpeg",
+      price: 5.99,
+      category: FoodCategory.sides,
+      availableAddons: [
+        Addon(name: "Extra butter", price: 1.29),
+        Addon(name: "Extra seasonings", price: 1.49),
+        Addon(name: "Extra sauce", price: 2.99),
+      ],
+    ),
+
+    // desserts
+    Food(
+      name: "Brownies",
+      description:
+          "Rich, fudgy brownies with a perfectly chewy texture and a hint of chocolate make every bite an irresistible delight.",
+      imagePath: "lib/images/desserts/brownies.jpeg",
+      price: 6.99,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra chocolate", price: 1.99),
+        Addon(name: "Whipped cream", price: 2.99),
+        Addon(name: "Extra nuts", price: 3.99),
+      ],
+    ),
+    Food(
+      name: "Jujubes",
+      description:
+          "Chewy, fruity jujubes in vibrant colors deliver a burst of sweetness with every bite, making them a fun and nostalgic treat.",
+      imagePath: "lib/images/desserts/jujubes.jpeg",
+      price: 3.99,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra jujubes", price: 2.49),
+        Addon(name: "Extra cup", price: 2.79),
+        Addon(name: "Extra nuts", price: 3.99),
+      ],
+    ),
+    Food(
+      name: "Short Bread",
+      description:
+          "Buttery, melt-in-your-mouth shortbread cookies with a delicate crumb and rich flavor make every bite a delightful indulgence.",
+      imagePath: "lib/images/desserts/shortbread.jpeg",
+      price: 8.99,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra butter", price: 2.19),
+        Addon(name: "Whipped cream", price: 2.99),
+        Addon(name: "Extra jam", price: 0.99),
+      ],
+    ),
+    Food(
+      name: "Tiramisu",
+      description:
+          "Layers of creamy mascarpone, coffee-soaked ladyfingers, and a dusting of cocoa come together in this indulgent tiramisu, creating a rich and irresistible dessert.",
+      imagePath: "lib/images/desserts/shortbread.jpeg",
+      price: 5.99,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra chocolate", price: 1.99),
+        Addon(name: "Whipped cream", price: 2.99),
+        Addon(name: "Fresh berries", price: 1.99),
+      ],
+    ),
+    Food(
+      name: "Watalappan",
+      description:
+          "Silky smooth watalappan, made with creamy coconut milk, rich jaggery, and aromatic spices, comes together in a decadent dessert that’s both comforting and irresistible.",
+      imagePath: "lib/images/desserts/watalappan.jpeg",
+      price: 4.99,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Coconut flakes", price: 2.99),
+        Addon(name: "Pice of jaggery", price: 0.99),
+        Addon(name: "Extra nuts", price: 3.99),
+      ],
+    ),
+
+    // drinks
+    Food(
+      name: "Avocado Drink",
+      description:
+          "Velvety avocado blended with sweetened condensed milk and a hint of vanilla creates a smooth, refreshing drink that’s both creamy and indulgent.",
+      imagePath: "lib/images/drinks/avacadodrink.jpeg",
+      price: 3.99,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Ice cream", price: 0.99),
+        Addon(name: "Churros", price: 1.99),
+        Addon(name: "Extra nuts", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Grape Soda",
+      description:
+          "Chilled grape soda, with its fizzy effervescence and sweet, fruity burst, offers a refreshing and delightful sip that’s both lively and satisfying.",
+      imagePath: "lib/images/drinks/grapesoda.jpeg",
+      price: 4.99,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Fruit pieces", price: 0.99),
+        Addon(name: "Gummies", price: 1.99),
+        Addon(name: "Extra soda", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Lemon Tea",
+      description:
+          "Freshly brewed lemon tea, with its tangy citrus kick and soothing warmth, delivers a refreshing and invigorating sip that’s both calming and revitalizing.",
+      imagePath: "lib/images/drinks/lemontea.jpeg",
+      price: 6.99,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Fruit pieces", price: 0.99),
+        Addon(name: "Scones", price: 1.49),
+        Addon(name: "Extra nuts", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Mocktail",
+      description:
+          "Vibrant mocktail, with its mix of fresh fruit juices, a splash of soda, and a hint of mint, offers a refreshing, fizzy burst that’s both lively and rejuvenating.",
+      imagePath: "lib/images/drinks/mocktail.jpeg",
+      price: 7.99,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Fruit pieces", price: 0.99),
+        Addon(name: "Cheese platter", price: 1.59),
+        Addon(name: "Extra nuts", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Raspberry Lemonade",
+      description:
+          "Refreshing raspberry lemonade, with its tangy citrus twist, sweet berry infusion, and crisp finish, delivers a burst of vibrant flavor that's both tart and invigorating.",
+      imagePath: "lib/images/drinks/raspberrylemonade.jpeg",
+      price: 7.99,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Fruit pieces", price: 0.99),
+        Addon(name: "Lemon bars", price: 0.99),
+        Addon(name: "Cheese platter", price: 1.59),
+      ],
+    ),
+  ];
+
+  /*
+
+  G E T T E R S
+
+  */
+
+  List<Food> get menu => _menu;
+
+  /*
+
+  O P E R A T I O N S
+  
+  */
+
+  // add to cart
+
+  // remove from cart
+
+  // get total price of cart
+
+  // get total number of items in cart
+
+  // clear cart
+
+  /*
+
+  H E L P E R S
+  
+  */
+
+  // generate a receipt
+
+  // format double value into money
+
+  // format list of addons into a string summary
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_current_location.dart';
 import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/components/my_food_tile.dart';
 import 'package:food_delivery_app/components/my_silver_app_bar.dart';
 import 'package:food_delivery_app/components/my_tab_bar.dart';
+import 'package:food_delivery_app/models/food.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,7 +24,8 @@ class _HomePageState extends State<HomePage>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController =
+        TabController(length: FoodCategory.values.length, vsync: this);
   }
 
   @override
@@ -29,49 +34,68 @@ class _HomePageState extends State<HomePage>
     super.dispose();
   }
 
+  // sort out and return a list of food items that belong to a specific category
+  List<Food> _filterMenuByCategory(FoodCategory category, List<Food> fullMenu) {
+    return fullMenu.where((food) => food.category == category).toList();
+  }
+
+  // return list of foods in given category
+  List<Widget> getFoodInThisCategory(List<Food> fullMenu) {
+    return FoodCategory.values.map((category) {
+      // get category menu
+      List<Food> categoryMenu = _filterMenuByCategory(category, fullMenu);
+
+      return ListView.builder(
+        itemCount: categoryMenu.length,
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          // get individual food
+          final food = categoryMenu[index];
+
+          // return food tile UI
+          return FoodTile(
+            food: food,
+            onTap: () {},
+          );
+        },
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
       body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) => [
-                MySilverAppBar(
-                  title: MyTabBar(tabController: _tabController),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Divider(
-                        indent: 25,
-                        endIndent: 25,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-
-                      // my current location
-                      const MyCurrentLocation(),
-
-                      // description box
-                      const MyDescriptionBox(),
-                    ],
-                  ),
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: MyTabBar(tabController: _tabController),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
+
+                // my current location
+                const MyCurrentLocation(),
+
+                // description box
+                const MyDescriptionBox(),
               ],
-          body: TabBarView(
+            ),
+          ),
+        ],
+        body: Consumer<Restaurant>(
+          builder: (context, restaurant, child) => TabBarView(
             controller: _tabController,
-            children: [
-              ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) => Text("first tab items"),
-              ),
-              ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) => Text("second tab items"),
-              ),
-              ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) => Text("third tab items"),
-              ),
-            ],
-          )),
+            children: getFoodInThisCategory(restaurant.menu),
+          ),
+        ),
+      ),
     );
   }
 }
