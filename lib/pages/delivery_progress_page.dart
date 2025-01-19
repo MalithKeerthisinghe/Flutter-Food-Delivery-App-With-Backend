9b4a50b596f0d6b4762e098119ca0e527e19481c
MalithKeerthisinghe <102132303+MalithKeerthisinghe@users.noreply.github.com>
2025-01-20 00:49:07 +0530

    connected to firebase
    auth_service.dart
    login_or_register.dart
    auth_gate.dart

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -11,9 +11,95 @@ class DeliveryProgressPage extends StatelessWidget {
         title: Text("Delivery in progress.."),
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
+      bottomNavigationBar: _buildBottomNavBar(context),
+      body: const Column(
         children: [
           MyReceipt(),
+        ],
+      ),
+    );
+  }
+
+  // Custom Bottom Nav Bar - Message / Call delivery river
+  Widget _buildBottomNavBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          // profile pic of driver
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.person),
+            ),
+          ),
+          const SizedBox(width: 10),
+
+          // driver details
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Malith Kaveesha",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              Text(
+                "Driver",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+
+          Row(
+            children: [
+              // message button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.message),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+
+              const SizedBox(width: 10),
+
+              // call button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.call),
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
